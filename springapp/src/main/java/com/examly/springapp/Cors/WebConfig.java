4357ed1baae49ec0467f0ab9dd08864b89d7b5d0
srija2006 <23cs237@skcet.ac.in>
2025-08-20 22:58:34 +0530

    updated endpoint

--- springapp/src/main/java/com/examly/springapp/Cors/WebConfig.java
+++ springapp/src/main/java/com/examly/springapp/Cors/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://health-coach-application-frontend-l.vercel.app")
+                        .allowedOrigins("https://health-coach-application-frontend-l.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // add OPTIONS
                         .allowedHeaders("*")
                         .exposedHeaders("Authorization") // allow browser to read token if needed
